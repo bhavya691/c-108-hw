565c3aadec41d01e665bbb76664808f137c72eb8
bhavya691 <70103543+bhavya691@users.noreply.github.com>
2021-11-22 21:27:10 +0530

    Add files via upload

---
+++ c-108/main.py
@@ -0,0 +1,5 @@
+import plotly.figure_factory as ff
+import pandas as pd
+df = pd.read_csv('data.csv')
+fig = ff.create_distplot([df['Avg Rating'].tolist()], ['Average Rating'], show_hist=False)
+fig.show()
